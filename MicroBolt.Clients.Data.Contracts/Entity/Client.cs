e6f5e515176257318defdb42354781e00827872e
akk0rd <vadim.riznik@gmail.com>
2018-06-24 17:04:21 +0300

    Data Contracts and dto
    Data Contracts and dto added

---
+++ MicroBolt.Clients.Data.Contracts/Entity/Client.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MicroBolt.Clients.Data.Contracts.Entity
+{
+    public class Client
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+        [Display(Name = "Date of Birthday")]
+        public DateTime BirthDay { get; set; }
+        [Display(Name = "Card Number")]
+        public string CardNumber { get; set; }
+        [Display(Name = "Adress of live")]
+        public string Adress { get; set; }
+        [Display(Name = "Age greatest 18?")]
+        public bool IsAdult { get; set; }
+        [Display(Name = "Currency")]
+        public string Currency { get; set; }
+    }
+}
---
+++ MicroBolt.Clients.Dto/GetClientDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroBolt.Clients.Dto
+{
+    class GetClientDto
+    {
+        public string Id { get; set; }
+    }
+}

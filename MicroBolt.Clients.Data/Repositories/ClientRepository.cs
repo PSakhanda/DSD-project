860685fcfb39fb0d6c50f8bf90b9cb729011def7
akk0rd <vadim.riznik@gmail.com>
2018-06-25 01:41:18 +0300

    Client Data

---
+++ MicroBolt.Clients.Data/Repositories/ClientRepository.cs
@@ -0,0 +1,48 @@
+﻿using MicroBolt.Clients.Data.Contracts;
+using MicroBolt.Clients.Data.Contracts.Entity;
+using MicroBolt.Clients.Data.Contracts.Repositories;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroBolt.Clients.Data.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly IStoreContext context;
+
+        public ClientRepository(IStoreContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Client> Get(string id)
+        {
+            return await this.context.Clients.Find(new BsonDocument("_id", new ObjectId(id))).FirstOrDefaultAsync();
+        }
+
+        public async Task<ICollection<Client>> GetMany()
+        {
+            return await this.context.Clients
+                .Find(new BsonDocument())
+                .ToListAsync();
+        }
+
+        public async Task Create(Client entity)
+        {
+            await this.context.Clients.InsertOneAsync(entity);
+        }
+
+        public async Task Update(Client entity)
+        {
+            await this.context.Clients.ReplaceOneAsync(new BsonDocument("_id", new ObjectId(entity.Id)), entity);
+        }
+        public async Task Delete(string id)
+        {
+            await this.context.Clients.DeleteOneAsync(new BsonDocument("_id", new ObjectId(id)));
+        }
+    }
+}
